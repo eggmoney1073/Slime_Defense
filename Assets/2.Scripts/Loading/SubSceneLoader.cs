57a858f718e4545f7e62da5a7b4c7bfb456f01dc
Eggmoney1073 <ahnhea0107@naver.com>
2026-05-06 23:59:17 +0900

    Fix SubScene

--- Assets/2.Scripts/Loading/SubSceneLoader.cs
+++ Assets/2.Scripts/Loading/SubSceneLoader.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class SubSceneLoader : MonoBehaviour
 {
-    [SerializeField] string sceneGUID;
+    [SerializeField] string _sceneGUID = "73eda0c52b1dddd429f0e1933666822d";
 
     void Start()
     {
         World world = World.DefaultGameObjectInjectionWorld;
 
-        if (world == null)
+        if (world == null || string.IsNullOrEmpty(_sceneGUID))
         {
-            Debug.LogError("World not initialized");
             return;
         }
 
-        Unity.Entities.Hash128 guid = new Unity.Entities.Hash128(sceneGUID);
+        Unity.Entities.Hash128 guid = new Unity.Entities.Hash128(_sceneGUID);
+        SceneSystem.LoadParameters loadParameters = new SceneSystem.LoadParameters();
 
-        SceneSystem.LoadSceneAsync(world.Unmanaged, guid);
+        SceneSystem.LoadSceneAsync(world.Unmanaged, guid, loadParameters);
     }
 }
