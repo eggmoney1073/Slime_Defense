3bd555859efc96981871622200c5f7e6f20f4e84
Eggmoney1073 <ahnhea0107@naver.com>
2026-05-07 00:09:12 +0900

    fix : subscene

--- Assets/2.Scripts/ECS/Authorings/SubSceneTriggerAuthoring.cs
+++ Assets/2.Scripts/ECS/Authorings/SubSceneTriggerAuthoring.cs
@@ -1,9 +1,6 @@
 using Unity.Entities;
 using Unity.Entities.Serialization;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 public struct SubSceneLoadRequest : IComponentData
 {
@@ -12,27 +9,18 @@ public struct SubSceneLoadRequest : IComponentData
 
 public class SubSceneTriggerAuthoring : MonoBehaviour
 {
-#if UNITY_EDITOR
-    [SerializeField] SceneAsset _subSceneAsset;
-#endif
+    [SerializeField] EntitySceneReference _sceneReference;
 
-    class Baker : Baker<SubSceneTriggerAuthoring>
+    class SubSceneTriggerBaker : Baker<SubSceneTriggerAuthoring>
     {
         public override void Bake(SubSceneTriggerAuthoring authoring)
         {
-#if UNITY_EDITOR
-            if (authoring._subSceneAsset == null)
-            {
-                return;
-            }
-
             Entity entity = GetEntity(TransformUsageFlags.None);
 
             AddComponent(entity, new SubSceneLoadRequest
             {
-                SceneRef = new EntitySceneReference(authoring._subSceneAsset)
+                SceneRef = authoring._sceneReference
             });
-#endif
         }
     }
 }
